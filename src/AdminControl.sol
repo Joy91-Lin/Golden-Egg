9fb690dce29e53d35cfa5aad81a6f0054005bc2f
chiaying-lin <joy.chiaying@gmail.com>
2024-01-06 16:24:51 +0800

    change modifier to function

--- src/AdminControl.sol
+++ src/AdminControl.sol
@@ -1,19 +1,23 @@
 pragma solidity ^0.8.21;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "./interfaces/IGoldenError.sol";
+import "./interfaces/IGoldenEvent.sol";
 
-contract AdminControl is Ownable{
-    error SenderMustBeAdmin(address caller);
+contract AdminControl is Ownable, IGoldenError, IGoldenEvent{
     mapping(address => bool) allowers;
 
     constructor()Ownable(msg.sender){
     }
 
-   modifier onlyAdmin() {
-        if(allowers[msg.sender])
-            _;
-        else
+   function onlyAdmin() public view {
+        if(!allowers[msg.sender])
             revert SenderMustBeAdmin(msg.sender);
+    }
+
+    function onlyContractOwner() public view {
+        if(msg.sender != owner()) 
+            revert OwnableUnauthorizedAccount(msg.sender);
     }
 
     function addAdmin(address account) public onlyOwner {
